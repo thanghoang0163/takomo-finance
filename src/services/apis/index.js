081908eb6d539103a0892a4b5df875b73627b405
Thang Hoang <hoanganhthang357@gmail.com>
2022-12-26 15:30:16 +0700

    Create Financial Instrument API

--- src/services/apis/index.js
+++ src/services/apis/index.js
@@ -2,5 +2,12 @@ import * as registerApis from "./register";
 import * as directoryApis from "./directory";
 import * as applicationApis from "./application";
 import * as phoneNumberApis from "./phone-number";
+import * as financeApis from "./finance";
 
-export { registerApis, directoryApis, applicationApis, phoneNumberApis };
+export {
+  registerApis,
+  directoryApis,
+  applicationApis,
+  phoneNumberApis,
+  financeApis,
+};
