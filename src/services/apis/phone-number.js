a5c6d32bacb9e15d05579aae2f4438d9bd05e30e
Thang Hoang <hoanganhthang357@gmail.com>
2022-12-30 14:24:04 +0700

    Delete Unnecessary API file

--- src/services/apis/phone-number.js
+++
@@ -1,9 +0,0 @@
-import request from "../index";
-
-export const prefixPhoneNumber = async () => {
-  const res = await request({
-    path: "/utils/settings",
-    method: "GET",
-  });
-  return res;
-};
