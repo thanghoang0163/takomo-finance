cc3f5cbc17dfa928c95e88d49b33610745d859a8
Thang Hoang <hoanganhthang357@gmail.com>
2023-01-04 14:58:17 +0700

    Edits Thank You Page

--- src/pages/thank-you/index.js
+++ src/pages/thank-you/index.js
@@ -1,6 +1,24 @@
+import parse from "@tiki.vn/mini-html-parser2";
+
 Page({
   data: {
-    btnText: "Về trang chủ",
+    htmlNodes: [],
+    label: "Chúc mừng quý khách đã hoàn thành​ thủ tục đăng ký tại takomo.vn",
+    text: "Vui lòng giữ liên lạc, kết quả của hồ sơ sẽ được nhân viên Takomo phản hồi trong ít phút qua điện thoại, hoặc email đã đăng ký.",
+  },
+
+  onLoad() {
+    const string = `Để thuận tiện cho việc theo dõi tình trạng khoản vay và các ưu đãi hấp
+    dẫn sắp tới, vui lòng truy cập <span class="mx-4x-small" style="color: #F82486">ứng dụng di động</span>hoặc
+    website của <span class="mx-4x-small" style="color: #F82486">Takomo.vn</span>​`;
+
+    parse(string, (err, htmlNodes) => {
+      if (!err) {
+        this.setData({
+          htmlNodes,
+        });
+      }
+    });
   },
 
   onShow() {
